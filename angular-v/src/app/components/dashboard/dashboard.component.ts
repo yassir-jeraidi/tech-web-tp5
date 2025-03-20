b2dbf793b182a4cb7adc0a3110d9b08711d3fb37
YassirJr <yassir.junior.03@gmail.com>
2025-03-20 00:37:23 +0000

    add the dashboard component

--- angular-v/src/app/components/dashboard/dashboard.component.ts
+++ angular-v/src/app/components/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -25,6 +26,8 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class DashboardComponent {
   isSidenavOpen = true;
+
+  constructor(protected authService : AuthService) {}
 
   toggleSidenav(): void {
     this.isSidenavOpen = !this.isSidenavOpen;
